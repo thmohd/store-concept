87cdad68919d5c80eab3eac5bdf65aaf9758ec99
Mohammad Althayabeh <althayabeh.mohammad@gmail.com>
2018-05-27 15:26:56 -0400

    seprate app code from library code and some other cleaning

--- index.js
+++ index.js
@@ -1,36 +1,29 @@
-// Sample Actions
-{
-  type: 'ADD_TODO',
-  todo: {
-    id: 0,
-    name: 'Learn Redux',
-    complete: false,
+//LIBRARY CODE
+function createStore(reducer) {
+
+  let state
+  let listeners = []
+
+  const getState = () => state
+  const subscribe = (listener) => {
+    listeners.push(listener)
+
+    return () => listeners.filter((l) => l !== listener )
+  }
+
+  const dispatch = (action) => {
+      state = reducer(state, action)
+      listeners.forEach((listener) => listener())
+  }
+
+  return {
+    getState,
+    subscribe,
+    dispatch
   }
 }
 
-{
-  type: 'REMOVE_TODO',
-  id: 0,
-}
-
-{
-  type: 'TOGGLE_TODO',
-  id: 0,
-}
-
-{
-  type: 'ADD_GOAL',
-  goal: {
-    id: 0,
-    name: 'Run a Marathon'
-  }
-}
-
-{
-  type: 'REMOVE_GOAL',
-  id: 0
-}
-
+//APP CODE
 
 /// Reducer Function (must be pure function)
 // take care of the todos
@@ -73,36 +66,6 @@ function app(state = {}, action){
   }
 }
 
-//Store
-function createStore(reducer) {
-
-  // The store should have four parts
-  // 1. The state
-  // 2. A way to get the state.
-  // 3. A way Listen to changes on the state.
-  // 4. A way to Update the state
-
-  let state
-  let listeners = []
-
-  const getState = () => state
-  const subscribe = (listener) => {
-    listeners.push(listener)
-
-    return () => listeners.filter((l) => l !== listener )
-  }
-
-  const dispatch = (action) => {
-      state = reducer(state, action)
-      listeners.forEach((listener) => listener())
-  }
-
-  return {
-    getState,
-    subscribe,
-    dispatch
-  }
-}
 
 /// Process example
 // define store
