e6336c7d5f70c0b5009eb379b8fea516ad2a9204
Mohammad Althayabeh <althayabeh.mohammad@gmail.com>
2018-05-27 13:00:52 -0400

    add getState Function

--- index.js
+++ index.js
@@ -3,3 +3,14 @@
 // 2. A way to get the state.
 // 3. A way Listen to changes on the state.
 // 4. A way to Update the state
+
+function createStore() {
+
+  let state
+
+  const getState = () => state
+
+  return {
+    getState,
+  }
+}
