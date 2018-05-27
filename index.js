f83054cb66a2e7f551dadd7348d184e55857701c
Mohammad Althayabeh <althayabeh.mohammad@gmail.com>
2018-05-27 13:03:27 -0400

    define subscribe function and handle listenrs

--- index.js
+++ index.js
@@ -7,10 +7,15 @@
 function createStore() {
 
   let state
+  let listeners = []
 
   const getState = () => state
+  const subscribe = (listener) => {
+    listeners.push(listener)
+  }
 
   return {
     getState,
+    subscribe
   }
 }
