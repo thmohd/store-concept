668777063d66600b3ce07b9ed4e02eb4f46543bc
Mohammad Althayabeh <althayabeh.mohammad@gmail.com>
2018-05-27 13:08:54 -0400

    define dispatch function and create sample actions for a TODO App

--- index.js
+++ index.js
@@ -1,10 +1,45 @@
-// The store should have four parts
-// 1. The state
-// 2. A way to get the state.
-// 3. A way Listen to changes on the state.
-// 4. A way to Update the state
+// Sample Actions
+{
+  type: 'ADD_TODO',
+  todo: {
+    id: 0,
+    name: 'Learn Redux',
+    complete: false,
+  }
+}
 
+{
+  type: 'REMOVE_TODO',
+  id: 0,
+}
+
+{
+  type: 'TOGGLE_TODO',
+  id: 0,
+}
+
+{
+  type: 'ADD_GOAL',
+  goal: {
+    id: 0,
+    name: 'Run a Marathon'
+  }
+}
+
+{
+  type: 'REMOVE_GOAL',
+  id: 0
+}
+
+
+//Store
 function createStore() {
+
+  // The store should have four parts
+  // 1. The state
+  // 2. A way to get the state.
+  // 3. A way Listen to changes on the state.
+  // 4. A way to Update the state
 
   let state
   let listeners = []
@@ -16,8 +51,13 @@ function createStore() {
     return () => listeners.filter((l) => l !== listener )
   }
 
+  const dispatch = (action) => {
+
+  }
+
   return {
     getState,
-    subscribe
+    subscribe,
+    dispatch
   }
 }
